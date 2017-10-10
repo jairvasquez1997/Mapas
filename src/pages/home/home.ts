a37436ea7f4e5380961ed65107435a95602c2553
vasquezaguirre <vasquezv2_97_10@hotmail.com>
2017-10-10 11:36:30 -0500

    commit 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
 
+declare var google;
+
+//@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  map: any;
+
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
 
   }
 
+  ionViewDidLoad(){
+    this.getPosition();
+  }
+  
+  getPosition():any{
+    this.geolocation.getCurrentPosition().then(response => {
+      this.loadMap(response);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  loadMap(position: Geoposition){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+
+    //creando un nuevo mapa pasando HTMLElement
+    let mapEle: HTMLElement = document.getElementById('map');
+
+    //creando latitud y longitud del objeto
+    let myLatLng = {lat: latitude, lng: longitude};
+
+    //mapa creado
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 15
+    });
+
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      let marker = new google.maps.Marker({
+        position: myLatLng,
+        map: this.map,
+        title: 'Aquí estoy!'
+      });
+      mapEle.classList.add('show-map');
+    });
+  }
 }
